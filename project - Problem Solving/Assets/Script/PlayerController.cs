99a6e5ca878c9612919dd29df98067e38c02acb4
Aldi Mulia Wijaya <deadreaperco@gmail.com>
2021-10-11 01:11:45 +0700

    step 4

---
+++ project - Problem Solving/Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //control
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+    public KeyCode leftButton = KeyCode.A;
+    public KeyCode rightButton = KeyCode.D;
+
+    //batasan
+    public float speed = 10.0f;
+    private Rigidbody2D rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = rigidBody2D.velocity;
+        
+        if (Input.GetKey(upButton)) velocity.y = speed; //atas
+        else if (Input.GetKey(downButton)) velocity.y = -speed; //kebawah
+        else velocity.y = 0.0f; //idle
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidBody2D.velocity = velocity;
+        
+        if (Input.GetKey(rightButton)) velocity.x = speed; //kekanan
+        else if (Input.GetKey(leftButton)) velocity.x = -speed; //kekiri
+        else velocity.x = 0.0f; //idle
+        // Masukkan kembali kecepatannya ke rigidBody2D.
+        rigidBody2D.velocity = velocity;
+    }
+}
