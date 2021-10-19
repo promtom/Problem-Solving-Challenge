4cceb07d3117cf9ee1b80691609e9d1cedf79d3a
Aldi Mulia Wijaya <deadreaperco@gmail.com>
2021-10-20 00:55:39 +0700

    Step 10
    Tapi lewatin step 9 jkarena step 9 lagi buntu kreativitas

---
+++ project - Problem Solving/Assets/Script/MouseInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+    public float speed = 4;
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+            SetTargerPosiotion();
+        
+        if(isMoving)
+            Move();
+    }
+
+    void SetTargerPosiotion()
+    {
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+        Debug.Log("TARGET LOCK = " + Input.mousePosition);
+        isMoving = true;
+    }
+
+    void Move()
+    {
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if(transform.position == targetPosition)
+        {
+            isMoving = false;
+        }
+    }
+}
